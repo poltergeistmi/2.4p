cb7d6c61fc90ec27b49b5727ee04412f29c490b5
poltergeistmi <poltergeistua@gmail.com>
2018-12-10 16:03:20 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,46 @@
+package com.company;
+
+
+        import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        Random rand = new Random();
+        rand.setSeed(System.currentTimeMillis());
+        for (int i=0; i <= 20; i++)
+        {
+            Integer r = rand.nextInt() % 256;
+            arrayList.add(r);
+        }
+
+        System.out.println("Початковий список:");
+        System.out.println(arrayList);
+        System.out.print("Введіть число: ");
+        int num = in.nextInt();
+
+        CompareInt compareInt = new CompareInt(num);
+        Collections.sort(arrayList, compareInt);
+
+        System.out.println("Переформатований список:");
+        System.out.println(arrayList);
+    }
+}
+
+class CompareInt implements Comparator<Integer>{
+
+    private Integer integer;
+
+    CompareInt(Integer integer){
+        this.integer = integer;
+    }
+
+    @Override
+    public int compare(Integer integer, Integer i2){
+        return integer - i2;
+    }
+}
+
